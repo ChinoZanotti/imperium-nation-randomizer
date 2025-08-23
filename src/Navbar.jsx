0f6335769119929f1a98b43dbf4212a5f50d8b09
Carlos Zanotti <cezanotti@gmail.com>
2025-08-23 12:15:24 -0300

    Add placeholder icons to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,13 +7,43 @@ function Navbar() {
           <ul>
             <li>
               <a href='#' id='drawBtn' className='icon-link'>
-                DRAW
+                <svg 
+                  width="50" 
+                  height="50" 
+                  viewBox="0 0 50 50" 
+                  fill="none" 
+                  xmlns="http://www.w3.org/2000/svg">
+                  <rect 
+                    width="50" 
+                    height="50" 
+                    fill="white"
+                  />
+                  <path 
+                    d="M25.4856 8L44.9711 41.75H6L25.4856 8Z" 
+                    fill="black"
+                  />
+                </svg>
               </a>
             </li>
             <li className='nav-divider'></li>
             <li>
               <a href='#' id='shuffleResetBtn'>
-                SHUF
+                <svg 
+                  width="50" 
+                  height="50" 
+                  viewBox="0 0 50 50" 
+                  fill="none" 
+                  xmlns="http://www.w3.org/2000/svg">
+                  <rect 
+                    width="50" 
+                    height="50" 
+                    fill="white"
+                  />
+                  <path 
+                    d="M25.4856 8L44.9711 41.75H6L25.4856 8Z" 
+                    fill="black"
+                  />
+                </svg>
               </a>
             </li>
           </ul>
